690a56fa1922472e78c3b5f29d8cbd2bc88ceebf
amanuelgthn <amanuelgthn@gmail.com>
2024-04-16 23:50:30 +0200

    FlippingMatrix

---
+++ FlippingMatrix/FlippingMatrix.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Flipping the Matrix"""
+
+#
+# Complete the 'flippingMatrix' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY matrix as parameter.
+#
+
+def flippingMatrix(matrix):
+    # Write your code here
+    
---
+++ FlippingMatrix/README.md
@@ -0,0 +1,1 @@
+FlippingMatrix
