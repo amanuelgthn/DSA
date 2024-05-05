345d5b63d1c40b2e93641e6415d0e229244fb0bc
amanuelgthn <amanuelgthn@gmail.com>
2024-05-05 23:44:11 +0200

    Update

--- listGame/listGame.py
+++ listGame/listGame.py
@@ -4,16 +4,20 @@
 if __name__ == '__main__':
     N = int(input())
     result = []
-    for i in range(12):
+    for i in range(N):
         line = input()
         line = line.split()
         if line[0] == 'insert':
-            if len(result) < int(line[1]) + 1:
-                result.append(line[2])
-            result[int(line[1])] = line[2]
+            result.insert(int(line[1]), int(line[2]))
         elif line[0] == 'append':
-            result.append(line[1])
+            result.append(int(line[1]))
         elif line[0] == 'remove':
-            result.remove(line[1])
+            result.remove(int(line[1]))
         elif line[0] == 'print':
             print(result)
+        elif line[0] == 'sort':
+            result.sort()
+        elif line[0] == 'pop':
+            result.pop()
+        elif line[0] == 'reverse':
+            result = result[::-1]
