3fe95111b88b62924e90ca9ac646af71b4f520c7
amanuelgthn <amanuelgthn@gmail.com>
2024-05-26 12:51:43 +0200

    increasingTriplet

---
+++ increasingTriplet/main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""main.py"""
+
+increasingTriplet = __import__('increasingTriplet').increasingTriplet
+
+nums1 = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+nums2 = [1,2,3,4,5]
+nums3 = [5,4,3,2,1]
+nums4 = [2,1,5,0,4,6]
+nums5 = [1,5,0,4,1,3]
+nums6 = [20,100,10,12,5,13]
+
+print(increasingTriplet(nums1))
+print(increasingTriplet(nums2))
+print(increasingTriplet(nums3))
+print(increasingTriplet(nums4))
+print(increasingTriplet(nums5))
+print(increasingTriplet(nums6))
