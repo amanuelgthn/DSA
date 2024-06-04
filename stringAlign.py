5faf63d52652e773744f8f29d22900624c7e7b17
amanuelgthn <amanuelgthn@gmail.com>
2024-06-04 23:57:10 +0200

    Update

--- stringAlign.py
+++ stringAlign.py
@@ -24,3 +24,4 @@ def stringAlignment(thickness):
     #Bottom Cone
     for i in range(thickness):
         print(((c*(thickness-i-1)).rjust(thickness)+c+(c*(thickness-i-1)).ljust(thickness)).rjust(thickness*6))
+        
