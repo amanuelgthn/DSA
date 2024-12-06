98e292c90588240d7c53171495356cbc97fd1ca5
amanuelgthn <amanuelgthn@gmail.com>
2024-12-06 23:42:09 +0100

    Updates

--- SortingAlgorithms/Quick_Sort/main.py
+++ SortingAlgorithms/Quick_Sort/main.py
@@ -5,5 +5,7 @@ QuickSort = __import__("QuickSort").QuickSort
 
 array1 = [234, 3, 6, 1, 90, 45, 23, 11]
 array2 = [1,2,3,4,5,6,7,8,9]
+array3 = [1, 2, 3, 4, 5, 6, 2, 7]
 QuickSort(array1)
 QuickSort(array2)
+QuickSort(array3)
